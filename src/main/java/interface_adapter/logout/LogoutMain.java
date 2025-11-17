fa57bc4b4d08f655f89364fd3afed5faa7ca948c
csc207 lab2 <mandyli.li@mail.utoronto.ca>
2025-11-17 15:28:10 -0500

    leaderboard

---
+++ src/main/java/interface_adapter/logout/LogoutMain.java
@@ -0,0 +1,10 @@
+package interface_adapter.logout;
+import use_case.Logout.LogoutInteractor;
+public class LogoutMain {public static void main(String[] args) {
+    LogoutViewModel viewModel = new LogoutViewModel();
+    LogoutPresenter presenter = new LogoutPresenter(viewModel);
+    LogoutInteractor interactor = new LogoutInteractor(presenter);
+    LogoutController controller = new LogoutController(interactor, viewModel);
+    new LogoutUI(controller);
+}
+}
