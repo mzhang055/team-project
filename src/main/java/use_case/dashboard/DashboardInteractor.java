47f069e0da248e0048c8872e32fc33180f968d58
Aykan Örsçelik <44747491+aykan2004@users.noreply.github.com>
2025-12-01 18:21:59 -0500

    Add files via upload

--- src/main/java/use_case/dashboard/DashboardInteractor.java
+++ src/main/java/use_case/dashboard/DashboardInteractor.java
@@ -1,43 +1,44 @@
 package use_case.dashboard;
 
-import data_access.MealDataAccessInterface;
-import data_access.UserDataAccessInterface;
+import data_access.InMemoryMealDataAccessObject;
+import data_access.InMemoryUserDataAccessObject;
 import entities.Meal;
 import entities.NutritionalInfo;
 import entities.User;
+import app.GoalsGuiMain;
 
 import java.util.List;
 
 public class DashboardInteractor implements DashboardInputBoundary{
-    private final UserDataAccessInterface userDataAccess;
-    private final MealDataAccessInterface mealDataAccess;
+    private final InMemoryUserDataAccessObject inMemoryUserDataAccessObject;
+    private final InMemoryMealDataAccessObject inMemoryMealDataAccessObject;
 
     private final DashboardOutputBoundary presenter;
 
-    public DashboardInteractor(UserDataAccessInterface userDataAccess, MealDataAccessInterface mealDataAccess, DashboardOutputBoundary presenter) {
-        this.userDataAccess = userDataAccess;
-        this.mealDataAccess = mealDataAccess;
+    public DashboardInteractor(InMemoryUserDataAccessObject inMemoryUserDataAccessObject, InMemoryMealDataAccessObject inMemoryMealDataAccessObject, DashboardOutputBoundary presenter) {
+        this.inMemoryUserDataAccessObject = inMemoryUserDataAccessObject;
+        this.inMemoryMealDataAccessObject = inMemoryMealDataAccessObject;
         this.presenter = presenter;
     }
 
     @Override
     public void loadDashboard(DashboardInputData input){
         String userId = input.getUserId();
-        User user = userDataAccess.getUser(userId);
-        List<Meal> meals = mealDataAccess.getMealsByUserId(userId);
+        User user = inMemoryUserDataAccessObject.getUser(userId);
+        List<Meal> meals = inMemoryMealDataAccessObject.getMealsByUserId(userId);
         DashboardOutputData output = getTotal(meals);
         presenter.updateDashboard(output);
     }
 
     private DashboardOutputData getTotal(List<Meal> meals){
         double calories = 0.0;
-        double remaining = 0.0;
         double proteins = 0.0;
         double fats = 0.0;
         double carbs = 0.0;
         double fibers = 0.0;
         double sugars = 0.0;
-        for(Meal meal : meals){
+
+        for (Meal meal : meals){
             NutritionalInfo x = meal.getNutritionalInfo();
             calories += x.getCalories();
             proteins += x.getProtein();
@@ -46,6 +47,15 @@ public class DashboardInteractor implements DashboardInputBoundary{
             fibers += x.getFiber();
             sugars += x.getSugar();
         }
+
+        double remaining = 0.0;
+        if (GoalsGuiMain.isActiveGoalSet()) {
+            remaining = GoalsGuiMain.getActiveTargetCalories() - calories;
+            if (remaining < 0) {
+                remaining = 0.0;
+            }
+        }
+
         return new DashboardOutputData(calories, remaining, proteins, carbs, fats, fibers, sugars);
     }
 }
